8c5598e185c66fee4bc52e5b3fbfdaf968b0b860
octaprog7 <goctaprog@gmail.com>
2022-10-09 23:14:24 +0500

    print sensor id

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from scd4x_sensirion import SCD4xSensirion
+from machine import I2C
+from sensor_pack.bus_service import I2cAdapter
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    ...
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    # Внимание!!!
+    # Замените id=1 на id=0, если пользуетесь первым портом I2C !!!
+    # Warning!!!
+    # Replace id=1 with id=0 if you are using the first I2C port !!!
+    i2c = I2C(id=1, freq=400_000)  # on Arduino Nano RP2040 Connect tested
+    adaptor = I2cAdapter(i2c)
+    # sensor
+    sen = SCD4xSensirion(adaptor)
+    sid = sen.get_id()
+    print(f"Sensor id words: {sid}")
+
 
